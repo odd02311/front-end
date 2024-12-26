747ae5991ba0ab11fb8e94084c9ea1407eac9a52
garry02311 <jaju1995@naver.com>
2024-12-26 12:43:29 +0900

    #16 jsp if-else
    자바스크립트 조건문 기초

---
+++ basic_front_end/part_1/Ch_9/05/js/main.js
@@ -0,0 +1,25 @@
+// let isShow = true;
+// let checked = false;
+
+// if (isShow) {
+//   console.log('Show');
+// }
+
+// if (checked) {
+//   console.log('Checked');
+// }
+
+let isShow = true;
+
+if (isShow) {
+  console.log('Show');
+} else {
+  console.log('Hide')
+}
+
+
+
+/**
+ * 조건문 : 조건의 결과(truthy, falsy)에 따라 다른 코드를 실행하는 구문. if-else
+ * 
+ **/
