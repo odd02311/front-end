28ccdfc1c6b5a57476cdd5d5f289fc68c378fc4e
garry02311 <jaju1995@naver.com>
2024-12-30 13:33:01 +0900

    #26 starbucks 예제 프로모션창 토글
    프로모션 창 틀 구성 및 디자인
    swiper을 이용해 애니메이션 처리
    swiperjs 자바스크립트 라이브러리:
    <link rel="stylesheet" href="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.css" />
    <script src="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js"></script>
    사용한 버전 6.8.4
    *최신 버전 7/8 버전과 사용법이 다르기때문에 유의
    Swiper를 사용할때 구조 작성은
    `<div class="swiper-container">
      <div class="swiper-wrapper">
        <div class="swiper-slide">1</div>
        <div class="swiper-slide">2</div>
        <div class="swiper-slide">3</div>
      </div>
    </div>`
    7/8 버전에서는 swipe-container가 아닌 swipe만으로 코드 작성
    
    css calc() 함수는 화면을 계산해서 구성할때 사용한다.
    커스텀으로 계산해서 px을 넣어도 되지만 정확하지 않을수도 있기 때문에
    calc(819px * 3 + 20px); 식으로 함수를 사용하면 손쉽게 원하는 px 단위의 화면 구성이 가능하다.
    
    요소를 화면의 축소/확대와 상관없이 정중앙에 표시하고 싶으면
    부모 요소에 postion: relative;를 넣고 자식 요소에 position: absolute;를 넣어준다.
    그리고 왼쪽 기준 left: 50%를 넣고 margin-left: -1238.5px;( 전체 width의 절반 값)을 넣어서 배치 시켜준다. 자주 쓰는 css 방식이다.
    여기서도 수동으로 계산한 px값을 넣기보다 위에 width에서 선언한 calc()함수 안의 값을 이용하여 margin-left: calc((819px * 3 + 20px)/ -2); 넣어도 된다.
    반복되는 css 속성이 있다면 다중선택자로 스타일을 선언하면 된다.
    
    또한 toggle material 변경 로직도 작성하여 펼치면 전환되게끔 디자인

--- basic_front_end/Starbucks/js/main.js
+++ basic_front_end/Starbucks/js/main.js
@@ -48,3 +48,47 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1 // 투명도 0에서 1로 나타나는 애니메이션 처리
   });
 });
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+
+}); // 자바스크립트 생성자
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백 10px
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000 // 500 = 0.5초
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 ㅍ페이지 번호 요소 제어 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+const promotionToggleIcon = document.querySelector('.toggle-promotion .material-icons')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+    promotionToggleIcon.classList.add("rotated");
+  } else {
+    // 표시 처리
+    promotionEl.classList.remove('hide');
+    promotionToggleIcon.classList.remove('rotated');
+  }
+});
